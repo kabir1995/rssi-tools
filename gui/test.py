70f9fe181dcb4da0f28ac3bff52728adf04e3115
Matheus Frata <matheus.frata@neoway.com.br>
2018-11-13 19:08:02 -0200

    Add tests of gui

---
+++ gui/test.py
@@ -0,0 +1,66 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton
+from PyQt5.QtGui import QIcon
+#******************************************************************
+import matplotlib
+matplotlib.use("Qt5Agg")
+#******************************************************************
+from PyQt5 import QtCore 
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import random
+ 
+class App(QMainWindow):
+ 
+    def __init__(self):
+        super().__init__()
+        self.left = 10
+        self.top = 10
+        self.title = 'RSSI Tools'
+        self.width = 640
+        self.height = 400
+        self.initUI()
+ 
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+ 
+        m = PlotCanvas(self, width=5, height=4)
+        m.move(0,0)
+ 
+        button = QPushButton('Start experiment', self)
+        button.setToolTip('This s an example button')
+        button.move(500,0)
+        button.resize(140,100)
+        button.clicked.connect(m.plot)
+ 
+        self.show()
+ 
+ 
+class PlotCanvas(FigureCanvas):
+ 
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+ 
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+ 
+        FigureCanvas.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self) 
+ 
+    def plot(self):
+        with open("seq.txt") as f: 
+            data = [int(x) for x in f.read().splitlines()] 
+        ax = self.figure.add_subplot(111)
+        ax.plot(data, 'r-')
+        ax.set_title('PyQt Matplotlib Example')
+        self.draw()
+ 
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
